4eac2fcaed4faa3596109b61aea62590143a2e65
Maddy1604 <mahehslavadne16400@gmail.com>
2024-10-07 17:39:34 +0530

    [Mahesh] Add: Adding loguru and making log file in log directory

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from loguru import logger
+
+logger.remove(0)
+logger.add("logs/file.log", level="INFO", rotation="100 MB")
 
 class Settings(BaseSettings):
     model_config=SettingsConfigDict(env_file='.env', extra="ignore")
@@ -22,4 +26,9 @@ class Settings(BaseSettings):
     MAIL_SSL_TLS : bool = False
     USE_CREDENTIALS : bool = True
     VALIDATE_CERTS : bool = True
+
+    # For user_services address 
+    AUTHORIZATION : str
+    CELERY_PATH : str
+
 settings = Settings()
