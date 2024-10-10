4df1bf4e5e6af555b6c050912378e21ddac8f630
Maddy1604 <mahehslavadne16400@gmail.com>
2024-10-10 11:20:50 +0530

    [Mahesh] Add: Adding new celery task for reminder

--- tasks.py
+++ tasks.py
@@ -2,10 +2,8 @@ from celery import Celery
 from settings import settings
 from user_services.email import send_verification_email, send_reminder_email
 from asgiref.sync import async_to_sync
-from celery.schedules import crontab
-from redbeat import RedBeatSchedulerEntry as Task
-from datetime import datetime 
-from pytz import timezone
+from loguru import logger
+
 
 celery = Celery(__name__, 
                 broker= settings.CELERY_PATH, 
@@ -13,28 +11,51 @@ celery = Celery(__name__,
                 broker_connection_retry_on_startup = True,
                 redbeat_lock_key=None,
                 beat_max_loop_interval=10,
-                beat_scheduler='redbeat.schedulers.RedBeatScheduler')
+                beat_scheduler='redbeat.schedulers.RedBeatScheduler',
+                enable_utc=False,)
 
 @celery.task
 def send_mail(email, verfit_link):
-    async_to_sync(send_verification_email)(email, verfit_link)
-    return "Success"
+    """
+    Description:
+    This task sends a verification email for a new user.
+    Parameters:
+    email: The email address of new user 
+    verify_link: The verification link for user
+    Returns:
+    str: A message indicating the result of the email sending operation as success.
+    """
+    logger.info(f"Sending verification email to user {email}")
+
+    try:
+        async_to_sync(send_verification_email)(email, verfit_link)
+        logger.info(f"Verification email successfully sent to {email}")
+        return "Success"
+    except Exception as error:
+        # Log any error that occurs
+        logger.error(f"Failed to send verification email to {email}: {str(error)}")
+        raise ValueError("Falied to perform task")
+
 @celery.task
 def reminder_email(email, note_id, note_title):
-    async_to_sync(send_reminder_email)(email, note_id, note_title)
-    return "Success"
-
-# def reminder_schedule(note):
-#     task_id = f"reminder_{note.id}"
-#     schedule_time = note.reminder.astimezone(pytz.UTC)
-
-#     # Remove existing task if updating
-#     celery.control.revoke(task_id, terminate=True)
-
-#     task = Task(name=f'{note_title["title"]}',
-#                         task='core.tasks.send_mail',
-#                         schedule=crontab(month_of_year=reminder.month,
-#                                          day_of_month=reminder.day,
-#                                          hour=reminder.hour,
-#                                          minute=reminder.minute),
-#                         app=celery, args=[])
+    """
+    Description:
+    This task sends a reminder email for a specific note identified by its ID.
+    Parameters:
+    note_id: The ID of the note for which the reminder email should be sent.
+    note_title: The title of note also be send with in email
+    Returns:
+    str: A message indicating the result of the email sending operation.
+    """
+    logger.info(f"Starting remainder email task for {email}")
+    
+    try:
+        # Send reminder email
+        async_to_sync(send_reminder_email)(email, note_id, note_title)
+        logger.info(f"Reminder email successfully sent to {email}")
+        return "success"
+    
+    except Exception as error:
+        # Log any error that occurs
+        logger.error(f"Failed to send reminder email to {email}: {str(error)}")
+        raise ValueError("Falied to perform task")
