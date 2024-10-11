87a625dc9bfe67a4dfe2de8ffda82543993b1dcc
Maddy1604 <mahehslavadne16400@gmail.com>
2024-10-11 18:00:33 +0530

    [Mahesh] Refact: Refactor the utils methods for get and delete.

--- notes_services/utils.py
+++ notes_services/utils.py
@@ -37,7 +37,7 @@ class JwtUtils():
         return data
     
     @classmethod
-    def get(cls, name):
+    def get(cls, name: str):
         """
         Description : Thus function is called for getting values in note caches
         Parameter:
@@ -55,13 +55,13 @@ class JwtUtils():
         Parameter:
         cls: By default it tkes
         name: for user id
-        ket : for note id
+        key : for note id
         """
         del_note = cls.r.hdel(name, key)
         return del_note
     
 # class for creating and storing caches of lables data in database = 1
-class JwtUtilsLables(JwtUtils):
+class JwtUtilsLabels(JwtUtils):
     # staring rediis chaching with configuration and database set at 1
     r = redis.Redis(host='localhost', port=6379, decode_responses=True, db = 1) 
 
